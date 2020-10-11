cdd2143b90f6b146902adcc201746482e31a4514
oribanda <oribanda2@gmail.com>
2020-10-11 11:04:42 +0900

    Initialize repository

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Calendar\Output\CalendarOutputView;
+
+class CalendarController extends Controller
+{
+    public function show()
+    {
+
+        $calendar = new CalendarOutputView(time());
+
+        return view('calendar', [
+            "calendar" => $calendar
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'CalendarController@show');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//祝日設定
+Route::get('/holiday_setting', 'Calendar\HolidaySettingController@form')->name("holiday_setting");
+Route::post('/holiday_setting', 'Calendar\HolidaySettingController@update')->name("update_holiday_setting");
+
+//臨時営業設定
+Route::get('/extra_holiday_setting', 'Calendar\ExtraHolidaySettingController@form')->name("extra_holiday_setting");
+Route::post('/extra_holiday_setting', 'Calendar\ExtraHolidaySettingController@update')->name("update_extra_holiday_setting");
